e1ad777bf71c3325d9f198920129c89491382262
asiergarrastazu <asier.garrastazu@alumnos.uneatlantico.es>
2026-01-09 16:01:37 +0100

    ajuste 3 arreglo los problemas de los ajustes 1 y 2 y termino de cambiar las variables dudosas

--- Entregas/GarrastazuAsier/src/calculadorageometria4.java
+++ Entregas/GarrastazuAsier/src/calculadorageometria4.java
@@ -1,137 +1,169 @@
+
 import java.util.Scanner;
 
-class calculadoraGeometria {
+class CalculadoraGeometria {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        String[] hist_f = new String[20];
-        double[] area = new double[20];
-        double[] perimetro = new double[20];
-        int hist_idx = 0;
-        double numeroPi = 3.14159;
-        
-        
+        Scanner scanner = new Scanner(System.in);
+
+        String[] historialFiguras = new String[20];
+        double[] historialAreas = new double[20];
+        double[] historialPerimetrosVolumenes = new double[20];
+        int indiceHistorial = 0;
+
+        final double PI = 3.14159;
+
         System.out.println("Calculadora Geometrica - Areas y Perimetros");
 
-        do{
+        while (true) {
             System.out.println("[1] Circulo");
             System.out.println("[2] Rectangulo");
             System.out.println("[3] Triangulo");
             System.out.println("[4] Cilindro");
             System.out.println("[5] Ver historial y Salir");
             System.out.print("Seleccione figura: ");
-            int x = sc.nextInt();
+            int opcionFigura = scanner.nextInt();
 
-            if (x == 5) {
+            if (opcionFigura == 5) {
                 System.out.println("Historial de Calculos");
-                double total_area = 0;
-                
-                for (int i = 0; i < hist_idx; i++) {
-                    System.out.println((i+1) + ". " + hist_f[i] + " -> Area: " + area[i] + ", Perimetro/Volumen: " + perimetro[i]);
-                    total_area += area[i];
+                double areaTotal = 0;
+
+                for (int i = 0; i < indiceHistorial; i++) {
+                    System.out.println(
+                        (i + 1) + ". " + historialFiguras[i] +
+                        " -> Area: " + historialAreas[i] +
+                        ", Perimetro/Volumen: " + historialPerimetrosVolumenes[i]
+                    );
+                    areaTotal += historialAreas[i];
                 }
-                System.out.println("Area total acumulada: " + total_area);
+
+                System.out.println("Area total acumulada: " + areaTotal);
                 System.out.println("Gracias por usar la calculadora.");
                 break;
             }
 
-            double Radio = 0;
-            double z = 0;
-            boolean f = false;
+            boolean calculoExitoso = false;
 
-            if (x == 1) {
+            if (opcionFigura == 1) { // Círculo
                 System.out.print("Radio: ");
-                Radio = sc.nextDouble();
-                if (Radio > 0) {
-                    z = numeroPi * Radio * Radio;
-                    double p = 2 * numeroPi * Radio;
-                    System.out.println("Area: " + z);
-                    System.out.println("Perimetro: " + p);
-                    f = true;
-                    if(hist_idx < 20) {
-                        hist_f[hist_idx] = "Circulo";
-                        area[hist_idx] = z;
-                        perimetro[hist_idx] = p;
-                        hist_idx++;
+                double radio = scanner.nextDouble();
+
+                if (radio > 0) {
+                    double areaCirculo = PI * radio * radio;
+                    double perimetroCirculo = 2 * PI * radio;
+
+                    System.out.println("Area: " + areaCirculo);
+                    System.out.println("Perimetro: " + perimetroCirculo);
+
+                    calculoExitoso = true;
+
+                    if (indiceHistorial < 20) {
+                        historialFiguras[indiceHistorial] = "Circulo";
+                        historialAreas[indiceHistorial] = areaCirculo;
+                        historialPerimetrosVolumenes[indiceHistorial] = perimetroCirculo;
+                        indiceHistorial++;
                     }
                 } else {
                     System.out.println("Radio invalido");
                 }
-            } else if (x == 2) {
+
+            } else if (opcionFigura == 2) { // Rectángulo
                 System.out.print("Base: ");
-                Radio = sc.nextDouble();
+                double base = scanner.nextDouble();
                 System.out.print("Altura: ");
-                z = sc.nextDouble();
-                if (Radio > 0 && z > 0) {
-                    double a = Radio * z;
-                    double p = 2 * (Radio + z);
-                    System.out.println("Area: " + a);
-                    System.out.println("Perimetro: " + p);
-                    f = true;
-                    if(hist_idx < 20) {
-                        hist_f[hist_idx] = "Rectangulo";
-                        area[hist_idx] = a;
-                        perimetro[hist_idx] = p;
-                        hist_idx++;
+                double altura = scanner.nextDouble();
+
+                if (base > 0 && altura > 0) {
+                    double areaRectangulo = base * altura;
+                    double perimetroRectangulo = 2 * (base + altura);
+
+                    System.out.println("Area: " + areaRectangulo);
+                    System.out.println("Perimetro: " + perimetroRectangulo);
+
+                    calculoExitoso = true;
+
+                    if (indiceHistorial < 20) {
+                        historialFiguras[indiceHistorial] = "Rectangulo";
+                        historialAreas[indiceHistorial] = areaRectangulo;
+                        historialPerimetrosVolumenes[indiceHistorial] = perimetroRectangulo;
+                        indiceHistorial++;
                     }
                 } else {
                     System.out.println("Dimensiones invalidas");
                 }
-            } else if (x == 3) {
+
+            } else if (opcionFigura == 3) { // Triángulo
                 System.out.print("Lado 1: ");
-                y = sc.nextDouble();
+                double x = scanner.nextDouble();
                 System.out.print("Lado 2: ");
-                z = sc.nextDouble();
+                double y = scanner.nextDouble();
                 System.out.print("Lado 3: ");
-                double w = sc.nextDouble();
-                if (y > 0 && z > 0 && w > 0 && (y + z > w && y + w > z && z + w > y)) {
-                    double s = (y + z + w) / 2;
-                    double a = Math.sqrt(s * (s - y) * (s - z) * (s - w));
-                    double p = y + z + w;
-                    System.out.println("Area: " + a);
-                    System.out.println("Perimetro: " + p);
-                    f = true;
-                    if(hist_idx < 20) {
-                        hist_f[hist_idx] = "Triangulo";
-                        area[hist_idx] = a;
-                        perimetro[hist_idx] = p;
-                        hist_idx++;
+                double z = scanner.nextDouble();
+
+                if (x > 0 && y > 0 && z > 0 &&
+                    x + y > z &&
+                    x + z > y &&
+                    y + z > x ) {
+
+                    double semiperimetro = (x  + y + z) / 2;
+                    double areaTriangulo = Math.sqrt(
+                        semiperimetro *
+                        (semiperimetro - x ) *
+                        (semiperimetro - y) *
+                        (semiperimetro - z)
+                    );
+                    double perimetroTriangulo = x  + y + z;
+
+                    System.out.println("Area: " + areaTriangulo);
+                    System.out.println("Perimetro: " + perimetroTriangulo);
+
+                    calculoExitoso = true;
+
+                    if (indiceHistorial < 20) {
+                        historialFiguras[indiceHistorial] = "Triangulo";
+                        historialAreas[indiceHistorial] = areaTriangulo;
+                        historialPerimetrosVolumenes[indiceHistorial] = perimetroTriangulo;
+                        indiceHistorial++;
                     }
                 } else {
                     System.out.println("Lados invalidos o no forman un triangulo");
                 }
-            } else if (x == 4) {
+
+            } else if (opcionFigura == 4) { // Cilindro
                 System.out.print("Radio: ");
-                y = sc.nextDouble();
+                double radio = scanner.nextDouble();
                 System.out.print("Altura: ");
-                z = sc.nextDouble();
-                
-                
-                if (y > 0 && z > 0) {
-                    double area_base = 3.14159 * y * y;
-                    double area_lateral = 2 * 3.14159 * y * z;
-                    double area_total = 2 * area_base + area_lateral;
-                    double vol = area_base * z;
-                    System.out.println("Area Superficial: " + area_total);
-                    System.out.println("Volumen: " + vol);
-                    f = true;
-                    if(hist_idx < 20) {
-                        hist_f[hist_idx] = "Cilindro";
-                        area[hist_idx] = area_total;
-                        perimetro[hist_idx] = vol;
-                        hist_idx++;
+                double altura = scanner.nextDouble();
+
+                if (radio > 0 && altura > 0) {
+                    double areaBase = PI * radio * radio;
+                    double areaLateral = 2 * PI * radio * altura;
+                    double areaTotal = 2 * areaBase + areaLateral;
+                    double volumen = areaBase * altura;
+
+                    System.out.println("Area Superficial: " + areaTotal);
+                    System.out.println("Volumen: " + volumen);
+
+                    calculoExitoso = true;
+
+                    if (indiceHistorial < 20) {
+                        historialFiguras[indiceHistorial] = "Cilindro";
+                        historialAreas[indiceHistorial] = areaTotal;
+                        historialPerimetrosVolumenes[indiceHistorial] = volumen;
+                        indiceHistorial++;
                     }
                 } else {
                     System.out.println("Dimensiones invalidas");
                 }
+
             } else {
                 System.out.println("Opcion no valida");
             }
 
-            if (f) {
+            if (calculoExitoso) {
                 System.out.println("Calculo completado.");
             }
-        sc.close();
-        while (true) { 
         }
+
+        scanner.close();
     }
+}
